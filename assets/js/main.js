56d9d890f3db8605bca43f2805da6832bc65a0fc
Bryce Anthony <branthony@davidson.edu>
2022-09-18 20:46:44 -0400

    added shapes and text

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,11 @@ Staaatliches Bauhaus (1919-1933)
 `;
 
 console.log(bauhaus);
+
+let screenLog = document.querySelector('#screen-log');
+document.addEventListener('mousemove', logKey);
+
+function logKey(e) {
+  screenLog.innerText = `
+    Coordinates X/Y: ${e.clientX}, ${e.clientY}`;
+}
